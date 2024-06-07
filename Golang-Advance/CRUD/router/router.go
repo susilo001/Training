28e9d26d77e6b3da3714b5847774178469261394
susilo001 <susiloodie@gmail.com>
2024-06-07 09:43:45 +0700

    User CRUD

---
+++ Golang-Advance/CRUD/router/router.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/susilo001/golang-advance/crud/handler"
+	"github.com/susilo001/golang-advance/crud/middleware"
+)
+
+func SetupRouter(r *gin.Engine) {
+	usersPublicEndpoint := r.Group("/users")
+	usersPublicEndpoint.GET("/:id", handler.GetUser)
+	usersPublicEndpoint.GET("/", handler.GetAllUsers)
+
+	usersPrivateEndpoint := r.Group("/users")
+	usersPrivateEndpoint.Use(middleware.AuthMiddleware())
+	usersPrivateEndpoint.POST("/", handler.CreateUser)
+	usersPrivateEndpoint.PUT("/:id", handler.UpdateUser)
+	usersPrivateEndpoint.DELETE("/:id", handler.DeleteUser)
+}
